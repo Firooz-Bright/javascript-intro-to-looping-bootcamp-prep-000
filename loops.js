6f1da0d98da45de622ba9541655956341c7b621f
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 20:11:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,7 @@
 function forLoop(Array){
- var arr=[Array]; 
   for(let i=0;i<25;i++){
     if(i===1){
-      arr.push("I am 1 strange loop");
+     Array.push("I am 1 strange loop");
     }
     else{
       arr.push(`I am ${i} strange loops`)
