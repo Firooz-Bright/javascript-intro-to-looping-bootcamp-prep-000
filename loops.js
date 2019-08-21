ecd7c7db48b32e718d5fc1fa49de149302121704
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 20:16:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(arr){
+function forLoop(array){
   for(var i=0;i<25;i++){
     if(i===1){
-    arr.push(` I am ${i} strange loop.`)
+    array.push(` I am ${i} strange loop.`)
     }
     else{
-      arr.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
   return arr
