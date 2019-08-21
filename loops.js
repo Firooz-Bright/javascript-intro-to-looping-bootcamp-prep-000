6b57c4485b2f92a46fffd83482fa6bf4c747b109
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 20:42:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,13 +17,3 @@ function whileLoop(num){
   return "done"
 }
 
-function doWhileLoop(num){
-  do {
-    console.log("I run once regardless.")
-  }while(incrementVariable()<num)
-}
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
