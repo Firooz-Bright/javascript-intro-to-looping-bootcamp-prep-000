6fa2f1e9790de63d77ffb79565ddcce197aeee34
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 20:45:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function incrementVariable() {
 }
 function doWhileLoop(num)
 do {
-  console.log("doo-bee-doo-bee-doo");
-} while (incrementVariable() < 5);
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
 ```
 
