3c67e8df87d5746833655cf5f404620574cf6987
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 20:42:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,15 @@ function whileLoop(num){
   return "done"
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
+}
+
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
